84ba393c457367e984c62af84130e433abd9b98f
Tran Ly <lytt2601@gmail.com>
2024-03-29 18:51:38 +0700

    test

---
+++ src/Main.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       /* System.out.printf("Hello and welcome!");
+        int i = 1;
+
+        for (; i <= 10; ) {
+            if (i==6)
+                break;
+            int product = 5 * i;
+            System.out.println("5*" + i + "= " + product);
+            i++;
+        }
+        System.out.println("End");
+    }
+}
+        /*int[] number = {1,2,3,4,5,6,7,8,9};
+        for(int i=number[1]; i<=6; i++){
+            System.out.println(number[i]);
+        }*/
+        /*int i = 1;
+        while (i<=10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("End");*/
+        /*Scanner scanner = new Scanner(System.in);
+        int number;
+        do{
+            System.out.println("Chon tinh nang:");
+            number = scanner.nextInt();
+            System.out.println("1. Them moi");
+            System.out.println("2. Sua");
+            System.out.println("3. Xoa");
+            System.out.println("4. Sap xep");
+            System.out.println("0. Thoat");
+        }while (number!=0);*/
+        /*int main ()
+        {
+                int a = 10;
+        do {
+            printf("Gia tri cua a la: %d/n", a);
+            a = a + 1;
+        }while( a < 15 );
+        printf("\n============================\n");
+        printf("VietJack chuc cac ban hoc tot! \");
+
+                return 0;
+        }
+        }*/
+    /*for (int i = 0; i < 10; i++){
+     if(i == 5)
+      continue;
+    System.out.println("i=" +i);
+    }
+    System.out.println("End of loop");*/
+        /*Scanner scanner = new Scanner(System.in);
+        int number;
+        do {
+            System.out.print("Enter a number:");
+            number = scanner.nextInt();
+            if (number < 2) {
+                System.out.println(number + " is not a prime");
+            } else {
+                int i = 2;
+                boolean check = true;
+                while (i < number) {
+                    if (number % i == 0) {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (check)
+                    System.out.println(number + " is a prime");
+                else
+                    System.out.println(number + " is not a prime");
+            }
+        } while (number != 0);
+    }
+
+    public int getSum(int num1, int num2) {
+
+}
+
+*/
+    }}
---
+++ src/Thuchanh.java
@@ -0,0 +1,79 @@
+public class Thuchanh {
+    public static void main(String[] args) {
+        /*string employeeName1 = "Khanh";
+        string employeeName2 = "Ha";
+        string employeeName3 = "Nam";
+        string employeeName4 = "An";
+        string employeeName5 = "Cuong";*/
+        /*String[] employeeNames = new String[10];
+        employeeNames[0] = "Khanh";
+        employeeNames [1] = "Nam";
+        employeeNames [2] = "An";
+        employeeNames [3] = "Cuong";
+
+        for (String name: employeeNames) {
+            System.out.println(name);*/
+        /*double[] numbers = new double[5];
+        numbers[0] = 8.9;
+        numbers[1] = 5.3;
+
+        for (double number: numbers) {
+            System.out.println(number);
+        }*/
+        //khởi tạo nhanh mảng
+        /*double[] numbers = {8.9, 5.3};
+        //numbers[0] = 8.9;
+        //numbers[1] = 5.3;
+
+        for (double number: numbers) {
+            System.out.println(number);
+        }*/
+        /*double[] myList = new double[10];
+        myList[0] = 5;
+        myList[1] = 6;
+        myList[2] = 7;
+        myList[3] = 8;
+        myList[4] = 9;
+        myList[5] = 10;
+        myList[6] = 11;
+        myList[7] = 12;
+        myList[8] = 13;
+        myList[9] = 14;
+
+        for (double X: myList) {
+            System.out.println(X);
+        }*/
+        /*int num1 = 30;
+        int num2 = 20;
+        int sum = new Thuchanh().getSum(num1, num2);
+        System.out.println(sum);*/
+        /*String mes = getMessage("Hello");
+        System.out.println(mes);
+        int num
+    }
+
+    private static String getMessage(String message){
+            return message;*/
+        /*int i = 5;
+        int j = 2;
+        int k = max(i, j);
+
+        System.out.println(
+                "The maxium of " + i + " and " + j + " is "  + k);
+    }
+    public static int max(int num1, int num2){
+        int result;
+
+        if (num1 > num2)
+            result = num1;
+        else
+            result = num2;
+        return result;*/
+
+        int a=5;
+        int b=10;
+        double kq = (double) a/b;
+        System.out.println(kq);
+    }
+
+}
